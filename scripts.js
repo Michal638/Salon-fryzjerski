51732b917952307f46d5bc8a0ac677a27b140204
Albert <amichalik7@gmail.com>
2025-01-25 11:21:09 +0100

    rejestracja1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function zarejestruj() {
+    const inmienazwisko = document.getElementById("inmienazwisko");
+    const email = document.getElementById("email");
+    const telefon = document.getElementById("telefon");
+    const haslo = document.getElementById("haslo");
+    const haslo2 = document.getElementById("haslo2");
+
+    
+}
